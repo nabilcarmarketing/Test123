c704eeeb11b0c539e062eced4b0274780f68e6c0
Firas <nabilcarmarketing@gmail.com>
2026-04-28 23:49:54 +0200

    update website

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,7 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-console.log("SUPABASE URL:", supabaseUrl);
-console.log("SUPABASE ANON KEY:", supabaseAnonKey);
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
@@ -12,7 +9,4 @@ if (!supabaseUrl || !supabaseAnonKey) {
   );
 }
 
-export const supabase = createBrowserClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 import { createServerClient } from "@supabase/ssr";
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse.next({ request });
+  const response = NextResponse.next();
 
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
