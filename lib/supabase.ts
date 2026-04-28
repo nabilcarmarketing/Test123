e2cffeff229a1433bc65356575bb9e81bf827c68
Firas <nabilcarmarketing@gmail.com>
2026-04-28 23:02:56 +0200

    update website

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl =
+  "https://utipkfkcllfthtslqk.supabase.co";
+
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1dGlwa2Zqa2NsbGZ0aHRzbHFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczODA1ODcsImV4cCI6MjA5Mjk1NjU4N30.4nnSICuJRzBxilsrNfxP1TvX7EzTtpoifZcoQNbU6h0";
 
 export const supabase = createClient(
   supabaseUrl,
