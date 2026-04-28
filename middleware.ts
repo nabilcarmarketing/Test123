25b4e9027b8dc1d5e568c6666210ddac0e2b7f10
Firas <nabilcarmarketing@gmail.com>
2026-04-28 22:41:09 +0200

    update website

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,6 @@ export async function middleware(request: NextRequest) {
   });
 
   const supabase = createServerClient(
-const supabase = createServerClient(
   "https://utipkfkcllfthtslqk.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1dGlwa2Zqa2NsbGZ0aHRzbHFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczODA1ODcsImV4cCI6MjA5Mjk1NjU4N30.4nnSICuJRzBxilsrNfxP1TvX7EzTtpoifZcoQNbU6h0",
     {
