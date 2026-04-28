455d69363ba786abc2ba8383ba3305f11aed2942
Firas <nabilcarmarketing@gmail.com>
2026-04-28 23:33:31 +0200

    debug supabase env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,10 +3,16 @@ import { createBrowserClient } from "@supabase/ssr";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+console.log("SUPABASE URL:", supabaseUrl);
+console.log("SUPABASE ANON KEY:", supabaseAnonKey);
+
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
     "Missing NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_ANON_KEY"
   );
 }
 
-export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createBrowserClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
